8f4de009179b6f869ee172a47bd3c0045d63d9df
17 <topvv17@gmail.com>
2018-11-26 14:46:17 +0200

    testing stuff

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+﻿var n = prompt('Размер ряда?', 0);
+
+function fib(n) {
+	var x = 0;
+	var y = 1;
+	var z;
+	var numbers = [0];
+	for(var i= 1; i < n; i++) {
+		z = x + y,
+		x = y,
+		y = z
+		numbers.push(z)
+	}
+	return numbers;
+}
+
+alert(fib(n));
