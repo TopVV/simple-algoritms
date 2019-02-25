e0d66b5a2114962791617f502a79acadc7442054
topVV <topvv17@gmail.com>
2019-02-25 16:07:38 +0200

    alpha version :D

--- screensaver/custom.js
+++ screensaver/custom.js
@@ -1,1 +1,15 @@
-Режим вывода команд на экран (ECHO) включен.
+var start = Date.now();
+
+
+var timer = setInterval(function(){
+    moveElement("falling-box-1");
+}, 20);
+
+
+function moveElement(elementId){
+    var timePassed = Date.now() - start;
+    if (timePassed > 2000) clearInterval(timer);
+    document.getElementById(elementId).style.top = timePassed / 5 + 'px';
+}
+
+
