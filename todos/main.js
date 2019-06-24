af1dd18fc38bcebd52efb4808629b334213cf34d
topvv <topvv17@gmail.com>
2019-06-24 23:25:55 +0300

    check/unchek and delete buttons works now

--- todos/main.js
+++ todos/main.js
@@ -5,43 +5,46 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     let newTodo = document.getElementById('todoInput').value;
-    console.log(newTodo);
 
-    // addNewTodo(newTodo);
-    addNewTodo2(newTodo);
+    if (newTodo == 0) {
+        return
+    };
+
+    addNewTodo(newTodo);
+
 
     document.getElementById('todoInput').value = '';
     addClickListner();
 });
 
-function addNewTodo(todoText) {
-    let todoItem = document.createElement("li");
-    let todoToggle = document.createElement("div");
-    let todoDelete = document.createElement("div");
-    let todoInner = document.createElement("div");
-    let todoCircle = document.createElement("i");
-    let todoXSign = document.createElement("i");
-    todoItem.className = 'todo-list__item';
-    todoToggle.className = 'todo-list__toggle';
-    todoDelete.className = 'todo-list__delete';
-    todoInner.className = 'todo-text';
-    todoCircle.className = 'far fa-circle';
-    todoXSign.className = 'fas fa-times';
-    todoItem.id = 'item' + todoCounter;
-    todoToggle.id = 'toggle' + todoCounter;
-    todoDelete.id = 'delete' + todoCounter;
-    todoInner.id = 'text' + todoCounter;
-    todoInner.innerHTML = todoText;
-    todoItem.appendChild(todoToggle);
-    todoItem.appendChild(todoDelete);
-    todoToggle.appendChild(todoCircle);
-    todoDelete.appendChild(todoXSign);
-    todoItem.appendChild(todoInner);
-    document.querySelector(".todo-list").appendChild(todoItem);
-    todoCounter++;
-}
+// --- OLD --- function addNewTodo(todoText) {
+//     let todoItem = document.createElement("li");
+//     let todoToggle = document.createElement("div");
+//     let todoDelete = document.createElement("div");
+//     let todoInner = document.createElement("div");
+//     let todoCircle = document.createElement("i");
+//     let todoXSign = document.createElement("i");
+//     todoItem.className = 'todo-list__item';
+//     todoToggle.className = 'todo-list__toggle';
+//     todoDelete.className = 'todo-list__delete';
+//     todoInner.className = 'todo-text';
+//     todoCircle.className = 'far fa-circle';
+//     todoXSign.className = 'fas fa-times';
+//     todoItem.id = 'item' + todoCounter;
+//     todoToggle.id = 'toggle' + todoCounter;
+//     todoDelete.id = 'delete' + todoCounter;
+//     todoInner.id = 'text' + todoCounter;
+//     todoInner.innerHTML = todoText;
+//     todoItem.appendChild(todoToggle);
+//     todoItem.appendChild(todoDelete);
+//     todoToggle.appendChild(todoCircle);
+//     todoDelete.appendChild(todoXSign);
+//     todoItem.appendChild(todoInner);
+//     document.querySelector(".todo-list").appendChild(todoItem);
+//     todoCounter++;
+// }
 
-function addNewTodo2(todoText) {
+function addNewTodo(todoText) {
     var todoItemTemplate = `
         <li class="todo-list__item" id="item${todoCounter}">
             <div class="todo-list__toggle" id="toggle${todoCounter}">
@@ -52,27 +55,54 @@ function addNewTodo2(todoText) {
             </div>
             <div class="todo-text" id="text${todoCounter}">${todoText}</div>
         </li>`;
-    
+
     var todoList = document.querySelector(".todo-list");
     todoList.innerHTML += todoItemTemplate;
 
     todoCounter++;
-}
+};
 
 function addClickListner() {
     let todoItemToggleList = document.getElementsByClassName('todo-list__toggle');
+    let todoItemDeleteList = document.getElementsByClassName('todo-list__delete');
     for (let i = 0; i < todoItemToggleList.length; i++) {
-        todoItemToggleList[i].addEventListener("click", check(i));
+        todoItemToggleList[i].addEventListener("click", checkTodo(+(todoItemToggleList[i].id.match(/\d+/)[0])));
+        todoItemDeleteList[i].addEventListener("click", deleteTodo(+(todoItemDeleteList[i].id.match(/\d+/)[0])));
     }
-}
+};
 
-function check(number) {
+function checkTodo(number) {
     return () => {
-        console.log('click', number);
-        document.getElementById('text' + number).style.textDecoration = "line-through";
+        function toCheck(n) {
+            document.getElementById('text' + n).style.textDecoration = "line-through";
+            modifyToggle('far fa-check-circle', n);
+            document.getElementById('toggle' + n).classList.add('checked');
+        };
+        function toUncheck(n) {
+            document.getElementById('text' + n).removeAttribute("style");
+            modifyToggle("far fa-circle", n);
+            document.getElementById('toggle' + n).classList.remove('checked');
+        };
+        function modifyToggle(className, n){
+            document.getElementById('toggle' + n).innerHTML = '';
+            let todoModified = document.createElement("i");
+            todoModified.className = className;
+            document.getElementById('toggle' + n).appendChild(todoModified);
+        };
+
+        if(document.getElementById('toggle' + number).classList.contains('checked')){
+            toUncheck(number);
+        } else {
+            toCheck(number)
+        };
+
     };
+};
+
+function deleteTodo(number){
+    return () => {
+        document.getElementById('item' + number).remove()
 }
+};
 
-console.log(document.getElementsByClassName('todo-list__toggle'));
 
-//.addEventListener("click", check);
